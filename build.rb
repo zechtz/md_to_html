652e17e1def364937b6d7b052d153b44e33ba795
Zachariah Ngonyani <zachariahn@gmail.com>
2016-08-18 09:08:21 +0300

    Finish first iteration

--- build.rb
+++ build.rb
@@ -1,7 +1,8 @@
 def main
   paths.each do |path|
     content = File.read(path)
-    compiled 
+    compiled = RDiscount.new(content).to_html 
+    new_path = path.sub(/\.md$/,".html")
     File.write(new_path, compiled)
   end
 end
